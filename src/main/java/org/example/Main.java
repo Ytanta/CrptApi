f4dc6a208be463e0d1697d01c9bc7d5a62a35235
Ytanta <121986594+Ytanta@users.noreply.github.com>
2025-09-17 17:22:25 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,46 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // 1. Создаём клиента
+        CrptApi api = new CrptApi(
+                "https://ismp.crpt.ru/api/v3",
+                CrptApi.TimeUnit.MINUTES,
+                10
+        );
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            CrptApi.AuthKeyResponse authKey = api.getAuthKey();
+            System.out.println("UUID: " + authKey.uuid);
+            System.out.println("Data to sign: " + authKey.data);
+
+            String signedData = signData(authKey.data);
+            System.out.println("Signed data: " + signedData);
+
+            CrptApi.AuthTokenResponse tokenResponse = api.getAuthToken(authKey.uuid, signedData);
+            String authToken = tokenResponse.authToken;
+            System.out.println("Auth token: " + authToken);
+
+            CrptApi.IntroduceGoodsDocument doc = new CrptApi.IntroduceGoodsDocument();
+            doc.participantInn = "7701234567";
+            doc.ownerInn = "7701234567";
+            doc.producerInn = "7701234567";
+            doc.productionDate = "2025-09-17";
+            doc.certificateDocument = "CERT";
+            doc.certificateDocumentDate = "2025-09-01";
+            doc.certificateDocumentNumber = "123456";
+
+            String docSignature = "fake-signature";
+
+            String response = api.createIntroduceGoodsDocument(doc, docSignature, authToken);
+            System.out.println("Response from ЧЗ: " + response);
+
+        } catch (CrptApi.ApiException e) {
+            System.err.println("Ошибка при работе с ЧЗ: " + e.getMessage());
         }
+    }
+
+    private static String signData(String data) {
+        return "signed_" + data;
     }
 }
